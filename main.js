038f5c3fb2f9dbc25e9891905c7216ebfae7c83b
Yuriy Kolodiy <macroch@yandex.ru>
2018-03-21 15:48:32 +0200

    add adaptive

--- main.js
+++ main.js
@@ -42,17 +42,18 @@ footer.addEventListener('mouseout', function () {
 });
 
 var videoImg = document.querySelector('.video__img');
+var img = document.querySelector('.video__img img');
 var videoMovie = document.querySelector('.video__movie');
 var video = document.querySelector('.video__movie video');
+
+video.style.height = getComputedStyle(img).height;
 
 videoImg.addEventListener('click', function () {
     this.style.display = 'none';
     videoMovie.style.display = 'block';
+    video.volume = 0.3;
     video.play();
 });
-// videoMovie.addEventListener('click', function() {
-//     video.play();
-// });
 
 videoMovie.addEventListener('click', function (e) {
     videoImg.style.display = 'block';
